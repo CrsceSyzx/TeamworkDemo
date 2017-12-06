0999ebe15d505c00c48149b3dda0c35b6e78c9cf
zhang123456677 <zbxgy1234566@163.com>
2017-12-06 18:41:44 +0800

    第一次提交

---
+++ TeamworkDemo/src/main/java/com/syzx/demo/teamwork/App.java
@@ -0,0 +1,14 @@
+package com.syzx.demo.teamwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! too" );
+    }
+}
