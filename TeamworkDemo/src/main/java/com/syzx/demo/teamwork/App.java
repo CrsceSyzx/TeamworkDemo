1396b670d7255677f5900e8b3334e4c587b9d41d
zhang123456677 <zbxgy1234566@163.com>
2017-12-07 07:58:14 +0800

    修改mian

--- TeamworkDemo/src/main/java/com/syzx/demo/teamwork/App.java
+++ TeamworkDemo/src/main/java/com/syzx/demo/teamwork/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! too" );
+        System.out.println( "Hello World! too too" );
     }
 }
