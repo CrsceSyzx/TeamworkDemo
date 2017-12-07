944f91aa7b1f8bb2f0d887bde32fd364760600dd
zhang123456677 <zbxgy1234566@163.com>
2017-12-07 08:07:05 +0800

    第三次提交

--- TeamworkDemo/src/main/java/com/syzx/demo/teamwork/App.java
+++ TeamworkDemo/src/main/java/com/syzx/demo/teamwork/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World! too" );
-        System.out.println( "Hello World! too too" );
+        System.out.println("ajsdkjasda");
     }
 }
